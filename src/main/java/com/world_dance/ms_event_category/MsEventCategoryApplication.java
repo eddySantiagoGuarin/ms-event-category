14a675f34d433353c259a57f6869c4711dbf6d94
eddySantiagoGuarin <eddyS.guarinc@uqvirtual.edu.co>
2026-08-20 15:58:50 -0500

    feature: Se agrego archivo dockerfile para dockerizar el proyecto y se cambiaron las versiones de las dependencias

--- src/main/java/com/world_dance/ms_event_category/MsEventCategoryApplication.java
+++ src/main/java/com/world_dance/ms_event_category/MsEventCategoryApplication.java
@@ -2,26 +2,26 @@ package com.world_dance.ms_event_category;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-@SpringBootApplication(scanBasePackages = {
-    "com.world_dance.ms_event_category",
-    "com.world_dance.wd_lib_common"
-})
-@EnableJpaRepositories(basePackages = {
-    "com.world_dance.ms_event_category.repository", 
-    "com.world_dance.wd_lib_common.repository"      
+@SpringBootApplication(exclude = {
+    MongoAutoConfiguration.class, 
+    MongoDataAutoConfiguration.class
 })
 @EntityScan(basePackages = {
-    "com.world_dance.ms_event_category.entity",    
-    "com.world_dance.wd_lib_common.entity"       
+    "com.world_dance.ms_event_category",
+    "com.world_dance.wd_lib_common.entity"
+})
+@EnableJpaRepositories(basePackages = {
+    "com.world_dance.ms_event_category",
+    "com.world_dance.wd_lib_common.repository"
 })
 public class MsEventCategoryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsEventCategoryApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MsEventCategoryApplication.class, args);
+    }
 }
